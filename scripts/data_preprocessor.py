36152b1b7f73ed9f20a23bc778a5926746a1d349
amare1990 <amaremek@gmail.com>
2025-01-16 15:45:54 +0300

    Refactor DataProcessor class to load API credentials from environment variables

--- scripts/data_preprocessor.py
+++ scripts/data_preprocessor.py
@@ -1,11 +1,19 @@
+import os
+BASE_DIR = os.path.join(__file__, '.env')
 from telethon import TelegramClient
+
+from dotenv import load_dotenv
+load_dotenv()
 
 
 
 class DataProcessor:
-    def __init__(self, api_id, api_hash, phone_number):
+    def __init__(self, phone_number):
         # Telegram client initialization
-        self.client = TelegramClient(phone_number, api_id, api_hash)
+
+        self.api_id = os.getenv('API_ID')
+        self.api_hash = os.getenv('API_HASH')
+        self.client = TelegramClient(phone_number, self.api_id, self.api_hash)
         self.channels = []  # List to store selected channels
         self.data = []  # List to store preprocessed data
 

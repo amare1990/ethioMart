22aca9d041c7fd02fef97ed7f3f8cef6f3ad1c83
amare1990 <amaremek@gmail.com>
2025-01-21 16:09:04 +0300

    Add fine_tuning_ner_pipeliner pipelined and notebook

--- scripts/fine_tuning_NER.py
+++ scripts/fine_tuning_NER.py
@@ -122,7 +122,7 @@ class NERModel:
         """
         training_args = TrainingArguments(
             output_dir='../data/results',          # output directory
-            num_train_epochs=3,              # number of training epochs
+            num_train_epochs=10,              # number of training epochs
             per_device_train_batch_size=8,   # batch size for training
             per_device_eval_batch_size=16,   # batch size for evaluation
             warmup_steps=500,                # number of warmup steps for learning rate scheduler
